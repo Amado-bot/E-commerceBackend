2b0299ae36f6b05cf78a446128deec4946e1bc69
unknown <amadocardenas0@gmail.com>
2021-06-11 22:18:51 -0600

    all linked in index though keys

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
+
 // Categories have many Products
+Category.hasMany(product,{
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  foreignKey: 'tag_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(product,{
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
